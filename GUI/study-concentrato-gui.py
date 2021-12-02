eaa8889805548acf16a16e0bacf96fbcbae3f658
KorryKatti <60970528+korrykatti@users.noreply.github.com>
2021-12-02 11:50:06 +0530

    Basic GUI MADE POG i thought to make it paid but nvm open source good

--- GUI/study-concentrato-gui.py
+++ GUI/study-concentrato-gui.py
@@ -1,10 +1,54 @@
 from tkinter import *
 from tkinter import ttk
+import webbrowser
+import datetime
+import time
+import random
 
 def testing():
     lol = int(min.get())
     minutes = lol * 60
-    print(minutes)
+    print("Starting up in 60 seconds so do whatever you want to and come in a minute and dont u dare move your mouse or the unexpected will happen lol i am not joking i am serious ")
+    time.sleep(60)
+    print("Haha your time ends less go")
+   
+    def func_test_1():
+        webbrowser.open("https://www.youtube.com/watch?v=rwSJwzE7lAg")
+
+    def func_test_2():
+        webbrowser.open("https://www.youtube.com/watch?v=fcZXfoB2f70")
+
+    def func_test_3():
+        webbrowser.open("https://www.youtube.com/watch?v=tZGpRd-t7jg")
+
+    def func_test_4():
+        webbrowser.open("https://www.youtube.com/watch?v=ZYzbalQ6Lg8")
+
+    root = Tk()
+
+    def current_position():
+        return [root.winfo_pointerx(), root.winfo_pointery()]
+
+    pos1 = current_position()
+
+    t_end = time.time() +minutes
+    while time.time() < t_end :
+
+        time.sleep(0.5)
+        pos2 = current_position()
+        if not pos1 == pos2:
+       # run a command:
+            my_list = [func_test_1, func_test_2, func_test_3, func_test_4]
+            random.choice(my_list)()
+        
+        pos1 = pos2
+
+    else:
+        webbrowser.open("https://www.youtube.com/watch?v=Zd9muK2M36c")
+
+
+
+
 
 root = Tk()
 root.title("Study Conectrato")
